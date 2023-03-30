7faa9a6dc74e64d16a3532317f982c3975414cfa
Fox Maikovich <fox@homebot.ai>
2023-03-30 14:59:42 -0600

    add testing and addresssit

--- src/sheets/testing.ts
+++ src/sheets/testing.ts
@@ -7,6 +7,32 @@ import {
   } from '@flatfile/configure'
   import * as hooks from '../datahooks/hooks'
   import * as emailValidator from 'email-validator'
+  var addressit = require("addressit")
   import { SmartDateField } from '../../examples/fields/SmartDateField'
 
-  export const Testing = new Sheet('testing', {}, {})
+  export const Testing = new Sheet('testing', {
+    'Subject Property Address': TextField({
+      validate: (addy: string) => {
+        const regex = /(\d)+(-?)[a-zA-Z]?\s+([a-zA-Z0-9])+/g
+        if (!regex.test(addy)) {
+          return [
+            new Message(
+              'Invalid propery address',
+              'error',
+              'validate'
+            )
+          ]
+        }
+      },
+      compute: (addy: string) => {
+        let parsed_addy = addressit(addy)
+        let final_addy = []
+
+        final_addy.push(parsed_addy.number, parsed_addy.street)
+
+        if (parsed_addy.unit != '')
+
+        return final_addy
+      }
+    })
+  }, {})
