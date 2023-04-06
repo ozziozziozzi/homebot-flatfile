3f409fa0bebcf976f31d0c8f82a3d6911de20897
Fox Maikovich <fox@homebot.ai>
2023-04-06 14:39:25 -0600

    update buyers to use resuable fields

--- src/sheets/buyers.ts
+++ src/sheets/buyers.ts
@@ -1,280 +1,194 @@
 import {
-    NumberField,
-    Sheet,
-    TextField,
-    OptionField,
-    Message,
-    BooleanField
-  } from '@flatfile/configure'
-  import * as hooks from '../datahooks/hooks'
-  import * as emailValidator from 'email-validator'
-  import { SmartDateField } from '../../examples/fields/SmartDateField'
+  NumberField,
+  Sheet,
+  TextField,
+  OptionField,
+  Message,
+  BooleanField
+} from '@flatfile/configure'
+import {
+  NameField,
+  PhoneField,
+  EmailField,
+  AddressField,
+  AddressZipField
+} from '../fields/reusable'
+import * as hooks from '../datahooks/hooks'
+import * as emailValidator from 'email-validator'
+import { SmartDateField } from '../../examples/fields/SmartDateField'
 
 export const Buyers = new Sheet('buyers', {
+  'First Name': NameField({
+    label: 'First Name',
+    required: true
+  }),
 
-    'First Name': TextField({
+  'Last Name': NameField({
+    label: 'Last Name',
+    required: true
+  }),
 
-        label: 'First Name',
-        required: true,
-        validate: (name:string) => {
-    
-            const regex = /^[\D\s]+$/g
-            if (!regex.test(name)) {
-                return [
-                    new Message(
-                        'Names cannot contain numbers',
-                        'error',
-                        'validate'
-                    )
-                ]
-            }
-        },
+  'Phone': PhoneField({
+    label: 'Phone'
+  }),
 
-        compute: (name: string) => {
-            const names = name.split(' ')
-      
-            for (let i = 0; i < names.length; i++) {
-              names[i] = names[i][0].toUpperCase() + names[i].substring(1).toLowerCase();
-            }
-      
-            return names.join(' ')
-          }
-        }),
+  'Email': EmailField({
+    label: 'Email',
+    required: true
+  }),
 
-        'Last Name': TextField({
+  'Zip Codes': TextField({
+    label: 'Zip Codes',
+    description: 'XXXXX (5 digit zip code)',
+    validate: (zip: string) => {
+      const regex = /^[0-9]{5}$/
+      if (!regex.test(zip)) {
+        return [
+          new Message(
+            'Invalid zip code',
+            'error',
+            'validate'
+          )
+        ]
+      }
+    },
+    compute: (zip: string) => {
+      if (zip.length > 5) {
+        return zip.slice(0, 5)
+      } else if (zip.length === 4) {
+        return '0' + zip
+      } else {
+        return zip
+      }
+    }
+  }),
 
-            label: 'Last Name',
-            required: true,
-            validate: (name:string) => {
-        
-                const regex = /^[\D\s]+$/g
-                if (!regex.test(name)) {
-                    return [
-                        new Message(
-                            'Names cannot contain numbers',
-                            'error',
-                            'validate'
-                        )
-                    ]
-                }
-            },
-    
-            compute: (name: string) => {
-                const names = name.split(' ')
-          
-                for (let i = 0; i < names.length; i++) {
-                  names[i] = names[i][0].toUpperCase() + names[i].substring(1).toLowerCase();
-                }
-          
-                return names.join(' ')
-              }
-            }),
+  'Price Point': TextField({
+    label: 'Price Point',
+    validate: (amount: string) => {
+      const numRegex = /(^\d+$)|(^\d*,?\d*,?\d*.?(\d{1,2})?$)/g
+      if (!numRegex.test(amount)) {
+        return [
+          new Message(
+            'Invalid numeric amount',
+            'error',
+            'validate'
+          )
+        ]
+      }
+    },
+    compute: (value: string) => {
+      return value.replace('$', '').trim()
+    }
+  }),
 
-            'Phone': TextField({
-                label: 'Phone',
-                description: 'XXX-XXX-XXXX',
-                validate: (phone: string) => {
-                  const regex = /^(\+?\d{1,2})?\s?([-.\s\(])?\d{3}\)?([-.\s\)])?\d{3}[-.\s]?\d{4}$/g
-                  if (!regex.test(phone)) {
-                    return [
-                      new Message(
-                        'Invalid phone number',
-                        'error',
-                        'validate'
-                      )
-                    ]
-                  }
-                },
-                compute: (phone: string) => {
-                  const regex = /^[\)\(\*\s-(N/?A)]+$/g
-                  if (regex.test(phone)) {
-                    return ''
-                  } else {
-                    return phone
-                  }
-                }
-              }),
+  'Language Preference': OptionField({
+    label: 'Language Preference',
+    description: 'English or Spanish',
+    options: {
+      english: 'en',
+      spanish: 'es'
+    }
+  }),
 
-              'Email': TextField({
-                label: 'Email',
-                required: true,
-                description: 'Tip! Double check the emails are complete, valid, and match the associated client on that row.',
-                validate: (email: string) => {
-                  const regex = /[\w-]+@([\w-]+\.)+[\w-]+/g
-                  const noneRegex = /^(none|noemail|fakeemail|na|N\/A)@/gi
-                  if (!regex.test(email) || noneRegex.test(email) || !emailValidator.validate(email)) {
-                    return [
-                      new Message(
-                        'Invalid email',
-                        'error',
-                        'validate'
-                      )
-                    ]
-                  } 
-                }
-              }),
+  'Max Loan Amount': TextField({
+    label: 'Max Loan Amount',
+    compute: (value: string) => {
+      return value.replace('$', '').trim()
+    }
+  }),
+
+  'Downpayment': TextField({
+    label: 'Downpayment',
+    compute: (value: string) => {
+      return value.replace('$', '').trim()
+    }
+  }),
 
 
-              'Zip Codes': TextField({
-                label: 'Zip Codes',
-                description: 'XXXXX (5 digit zip code)',
-                validate: (zip: string) => {
-                  const regex = /^[0-9]{5}$/
-                  if (!regex.test(zip)) {
-                    return [
-                      new Message(
-                        'Invalid zip code',
-                        'error',
-                        'validate'
-                      )
-                    ]
-                  }
-                },
-                compute: (zip: string) => {
-                  if (zip.length > 5) {
-                    return zip.slice(0, 5)
-                  } else if (zip.length === 4) {
-                    return '0' + zip
-                  } else {
-                    return zip
-                  }
-                }
-              }),
+  'Max Home Price': TextField({
+    label: 'Max Home Price',
+    compute: (value: string) => {
+      return value.replace('$', '').trim()
+    }
+  }),
 
-              'Price Point': TextField({
-                label: 'Price Point',
-                validate: (amount: string) => {
-                  const numRegex = /(^\d+$)|(^\d*,?\d*,?\d*.?(\d{1,2})?$)/g
-                  if (!numRegex.test(amount)) {
-                    return [
-                      new Message(
-                        'Invalid numeric amount',
-                        'error',
-                        'validate'
-                      )
-                    ]
-                  }
-                },
-                compute: (value: string) => {
-                  return value.replace('$', '').trim()
-                }
-              }),
+  'Estimated Rate': TextField({
+    label: 'Estimated Rate',
+    compute: (value: string) => {
+      const rate = value.replace('%', '').trim()
+      if (rate.match(/^0+.?0{0,2}$/g)) {
+        return '0.01'
+      } else {
+        return value
+      }
+    }
+  }),
 
-              'Language Preference': OptionField({
-                label: 'Language Preference',
-                description: 'English or Spanish',
-                options: {
-                  english: 'en',
-                  spanish: 'es'
-                }
-              }),
+  'Loan Type': TextField({
+    label: 'Loan Type'
+  }),
 
-              'Max Loan Amount': TextField({
-                label: 'Max Loan Amount',
-                compute: (value: string) => {
-                  return value.replace('$', '').trim()
-                }
-              }),
+  'Loan Term Months': OptionField({
+    label: 'Loan Term Months',
+    matchStrategy: 'exact',
+    options: {
+      '120': '120',
+      '180': '180',
+      '240': '240',
+      '264': '264',
+      '300': '300',
+      '360': '360'
+    },
+    compute: (value: any) => {
+      const labels = ['120', '180', '240', '264', '300', '360']
+      let term = value.toString()
+      if (!labels.includes(term)) {
+        term = term.replace(/\D/g, '').trim()
+        if (labels.includes(String(term))) {
+          return term
+        }
+        if (term <= 30) {
+          const product : number = Number(term) * 12
+          return String(product)
+        } else {
+          return term
+        }
+      } else {
+        return term
+      }
+    }
+  }),
 
-              'Downpayment': TextField({
-                label: 'Downpayment',
-                compute: (value: string) => {
-                  return value.replace('$', '').trim()
-                }
-              }),
+  'Expires At': SmartDateField({
+    label: 'Expires At',
+    locale: 'en',
+    description: 'Smart dates',
+    formatString: 'M/d/yyyy'
+  }),
 
+  'Loan Adjustable': BooleanField({
+    description: "Is the loan adjustable?",
+  }),
 
-              'Max Home Price': TextField({
-                label: 'Max Home Price',
-                compute: (value: string) => {
-                  return value.replace('$', '').trim()
-                }
-              }),
+  'Pre Approved': BooleanField({
+    description: "Is the loan pre approved?",
+  }),
 
-              'Estimated Rate': TextField({
-                label: 'Estimated Rate',
-                compute: (value: string) => {
-                  const rate = value.replace('%', '').trim()
-                  if (rate.match(/^0+.?0{0,2}$/g)) {
-                    return '0.01'
-                  } else {
-                    return value
-                  }
-                }
-              }),
-
-
-              'Loan Type': TextField({
-                label: 'Loan Type'
-              }),
-
-              'Loan Term Months': OptionField({
-                label: 'Loan Term Months',
-                matchStrategy: 'exact',
-                options: {
-                  '120': '120',
-                  '180': '180',
-                  '240': '240',
-                  '264': '264',
-                  '300': '300',
-                  '360': '360'
-                },
-                compute: (value: any) => {
-                  const labels = ['120', '180', '240', '264', '300', '360']
-                  let term = value.toString()
-                  if (!labels.includes(term)) {
-                    term = term.replace(/\D/g, '').trim()
-                    if (labels.includes(String(term))) {
-                      return term
-                    }
-                    if (term <= 30) {
-                      const product : number = Number(term) * 12
-                      return String(product)
-                    } else {
-                      return term
-                    }
-                  } else {
-                    return term
-                  }
-                }
-              }),
-
-              'Expires At': SmartDateField({
-                label: 'Expires At',
-                locale: 'en',
-                description: 'Smart dates',
-                formatString: 'M/d/yyyy'
-              }),
-              
-              'Loan Adjustable': BooleanField({
-                description: "Is the loan adjustable?",
-              }),
-
-              'Pre Approved': BooleanField({
-                description: "Is the loan pre approved?",
-              }),
-
-              'Loan ARM Years': TextField({
-                label: 'Loan ARM Years',
-                compute: (value: string) => {
-                  return value.replace('$', '').trim()
-                }
-              }),
-
-
-
-            },
-
-            {
-                allowCustomFields: true,
-                recordCompute: (record) => {
-                  hooks.conditionalFormatting(record)
-                  hooks.highlyEncouraged(record)
-                  hooks.miscellaneousPhoneRemover(record)
-                  hooks.coborrowerEmailCheck(record)
-                  return record
-                }
-              }
-              
-           
-              )
+  'Loan ARM Years': TextField({
+    label: 'Loan ARM Years',
+    compute: (value: string) => {
+      return value.replace('$', '').trim()
+    }
+  }),
+},
+{
+  allowCustomFields: true,
+  recordCompute: (record) => {
+    hooks.conditionalFormatting(record)
+    hooks.highlyEncouraged(record)
+    hooks.miscellaneousPhoneRemover(record)
+    hooks.coborrowerEmailCheck(record)
+    return record
+  }
+})
